72447e188ab33b86b3e2180dee8d923c98c38bba
Dominic Salmieri <dsalmieri@olin.edu>
2024-04-25 11:00:00 -0400

    Barebones scaffold

---
+++ src/main/kotlin/NearestNeighbor.kt
@@ -0,0 +1,35 @@
+package org.example
+
+class NearestNeighbor {
+    val alphabet: List<Char> = TODO()
+    val vocab: List<String> = TODO()
+    val nBands: Int = 10
+
+    fun shingle(string: String): List<String> {
+        TODO()
+    }
+
+    fun sparseVector(string: String): List<Boolean> {
+        TODO()
+    }
+
+    fun denseVector(sparse: List<Boolean>): List<Boolean> {
+        TODO()
+    }
+
+    fun bin(dense: List<Boolean>) {
+        TODO()
+    }
+
+    fun add(string: String) {
+        TODO()
+    }
+
+    fun findCandidates(string: String) {
+        TODO()
+    }
+
+    fun findClosest(string: String) {
+        TODO()
+    }
+}
